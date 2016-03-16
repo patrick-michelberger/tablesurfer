aabc90dc051a79cac7eb3e7661a1e7f014de96ee
patrick_michelberger <pmichelberger@gmx.de>
2016-03-16 22:01:43 +0100

    translate tags added

--- client/app/translations.js
+++ client/app/translations.js
@@ -1,4 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
+    gettextCatalog.setStrings('de', {"Admin":"Administrator","Hello":"Hallo","Home":"Startseite","Login":"Login","Logout":"Logout","Sign up":"Anmelden","The Cooking-Community <b>for Students!</b>":"Die Koch-Community für Studenten"});
 /* jshint +W100 */
 }]);
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -5,19 +5,22 @@ class NavbarController {
     //end-non-standard
 
     constructor(Auth, gettextCatalog) {
-    	this.gettextCatalog = gettextCatalog;
+        this.gettextCatalog = gettextCatalog;
         this.isLoggedIn = Auth.isLoggedIn;
         this.isAdmin = Auth.isAdmin;
         this.getCurrentUser = Auth.getCurrentUser;
+        this.currentLanguage = this.gettextCatalog.getCurrentLanguage();
     }
 
     changeLanguage(code) {
         this.gettextCatalog.setCurrentLanguage(code);
+        console.log("currentLanguage: ", this.gettextCatalog.getCurrentLanguage());
+        this.currentLanguage = this.gettextCatalog.getCurrentLanguage();
     }
 
 }
 
-NavbarController.$inject = ['gettextCatalog'];
+NavbarController.$inject = ['Auth', 'gettextCatalog'];
 
 angular.module('tablesurferApp')
     .controller('NavbarController', NavbarController);
