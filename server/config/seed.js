34749b60692aefeb7d0b7d74bf223dec81b193d9
patrick_michelberger <pmichelberger@gmx.de>
2016-03-17 08:11:14 +0100

    seed updated

--- server/config/seed.js
+++ server/config/seed.js
@@ -3,57 +3,60 @@
  * to disable, edit config/environment/index.js, and set `seedDB: false`
  */
 
+/**
+ * Populate DB with sample data on server start
+ * to disable, edit config/environment/index.js, and set `seedDB: false`
+ */
+
 'use strict';
-import Thing from '../api/thing/thing.model';
-import User from '../api/user/user.model';
 
-Thing.find({}).remove()
-  .then(() => {
-    Thing.create({
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Bower, Grunt, Babel, Karma, ' +
-             'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-             'Stylus, Sass, and Less.'
+var University = require('../api/university/university.model');
+var City = require('../api/city/city.model');
+var User = require('../api/user/user.model');
+
+University.find({}).remove(function() {
+    University.create({
+        "name": "LMU München",
+        "campusMails": ["lmu.de"]
     }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-             'AngularJS, and Node.'
+        "name": "TU München",
+        "campusMails": ["tum.de"]
     }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep ' +
-             'tests alongside code. Automatic injection of scripts and ' +
-             'styles into your index.html'
-    }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more ' +
-             'code reusability and maximum scalability'
-    }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript ' +
-             'payload, minifies your scripts/css/images, and rewrites asset ' +
-             'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-             'and openshift subgenerators'
+        "name": "Universität Hamburg",
+        "campusMails": ["uh.edu"]
+    }, function(err, LMU, TUM, UH) {
+        City.find({}).remove(function() {
+            City.create({
+                    "name": "Munich",
+                    "latitude": "48.8",
+                    "longitude": "11.35",
+                    "universities": [LMU, TUM]
+                }, {
+                    "name": "Hamburg",
+                    "latitude": "53.33",
+                    "longitude": "10.2",
+                    "universities": [UH]
+                },
+                function(err, Munich, Hamburg) {
+
+                });
+        });
     });
-  });
+});
 
-User.find({}).remove()
-  .then(() => {
+User.find({}).remove(function() {
     User.create({
-      provider: 'local',
-      name: 'Test User',
-      email: 'test@example.com',
-      password: 'test'
+        provider: 'local',
+        name: 'Test User',
+        email: 'test@test.com',
+        password: 'test'
     }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin'
-    })
-    .then(() => {
-      console.log('finished populating users');
+        provider: 'local',
+        role: 'admin',
+        name: 'Admin',
+        email: 'admin@admin.com',
+        password: 'admin'
+    }, function() {
+        console.log('finished populating users');
     });
-  });
+});
