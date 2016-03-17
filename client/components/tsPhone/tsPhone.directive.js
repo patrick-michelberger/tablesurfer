5dd415c4144c0704dec522d34e95696306c45ef0
patrick_michelberger <pmichelberger@gmx.de>
2016-03-17 08:51:23 +0100

    phone directive added

--- client/components/tsPhone/tsPhone.directive.js
+++ client/components/tsPhone/tsPhone.directive.js
@@ -7,6 +7,61 @@ angular.module('tablesurferApp')
       restrict: 'E',
       scope: true,
       link: function (scope, element, attrs) {
+
+        var currentUser = scope.currentUser = Auth.getCurrentUser();
+
+        scope.hasPhonecode = function() {
+          if (currentUser.verifiedPhone === false) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        scope.hasPhone = function() {
+          return (!currentUser.phone || currentUser.phone.length < 1 || !currentUser.verifiedPhone) ? false : true;
+        };
+
+        // ACTIONS
+      	scope.saveNumber = function() {
+          scope.isLoading = true;
+      		Auth.changePhone(scope.currentUser.phone)
+	        .then( function(user) {
+            scope.isLoading = false;
+            Auth.currentUser
+	        	scope.phoneChanged = true;
+	        	$timeout(function() {
+	        		scope.phoneChanged = false;
+	        	}, 1500);
+	        })
+      	};
+
+        scope.submit = function() {
+          scope.isLoading = true;
+
+          Auth.verifyPhone(scope.currentUser.phonecode).then(function(response) {
+
+            scope.isLoading = false;
+            scope.currentUser.verifiedPhone = true;
+
+          }).catch(function(err) {
+
+            scope.isLoading = false;
+            scope.hasError = true;
+            delete scope.currentUser.phonecode;
+
+            $timeout(function() {
+              scope.hasError = false;
+            }, 1500);
+
+          });
+
+        };
+
+        scope.changeNumber = function() {
+          delete scope.currentUser.phone;
+          delete scope.currentUser.verifiedPhone;
+        };
       }
     };
   });
