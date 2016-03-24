14cea515ec1600549d7b0c10653e8a496ee2c601
patrick_michelberger <pmichelberger@gmx.de>
2016-03-24 18:08:12 +0100

    weekday selection changed

--- client/components/tsWeekdays/tsWeekdays.directive.js
+++ client/components/tsWeekdays/tsWeekdays.directive.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('tablesurferApp')
-    .directive('tsWeekdays', function() {
+    .directive('tsWeekdays', function(Auth) {
         return {
             templateUrl: 'components/tsWeekdays/tsWeekdays.html',
             restrict: 'EA',
             link: function(scope, element, attrs) {
+
+                var currentUser = Auth.getCurrentUser();
+
+                scope.weekdays = {};
+
+                currentUser.weekdays.forEach(function(day) {
+                    scope.weekdays[day] = true;
+                });
+
                 scope.preferences = [];
 
                 scope.$watchCollection('weekdays', function() {
@@ -13,9 +22,21 @@ angular.module('tablesurferApp')
                     angular.forEach(scope.weekdays, function(value, key) {
                         if (value) {
                             scope.preferences.push(key);
+                            Auth.changeWeekdays(scope.preferences);
                         }
                     });
                 });
+
+                scope.savePreferences = function() {
+                    Auth.changeWeekdays(scope.preferences)
+                        .then(() => {
+                            scope.$emit('user:changed');
+                        })
+                        .catch(err => {
+                            err = err.data;
+                            console.log("Err: ", err);
+                        });
+                };
             }
         };
     });
