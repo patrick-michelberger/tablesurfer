433ba84684ebd44855d7c190b2303d43846a811c
patrick_michelberger <pmichelberger@gmx.de>
2016-03-26 17:00:28 +0100

    dashboard changed

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -2,101 +2,52 @@
 
 angular.module('tablesurferApp')
     .controller('DashboardCtrl', function($rootScope, $scope, $state, $timeout, Auth) {
-        $scope.isLoggedIn = Auth.isLoggedIn;
-        $scope.getCurrentUser = Auth.getCurrentUser;
-        $scope.isEmailVerified = Auth.isEmailVerified;
-        $scope.submitted = false;
-        $scope.user = Auth.getCurrentUser() || {};
-        $scope.currentProgress = updateProgressBar();
 
 
-        $timeout(function() {
-            if (Auth.getCurrentUser().verified && !Auth.getCurrentUser().verifiedPhone) {
-                $rootScope.Ui.turnOn('phoneModal');
-            }
-        }, 0);
+        if (!Auth.getCurrentUser().registrationCompleted) {
+            $state.go('onboarding');
+        }
 
-        $scope.$watch('user', function() {
+        /*
             $scope.currentProgress = updateProgressBar();
-        });
 
-        var errors = {};
-
-        $scope.changeFirstName = function(form) {
-            $scope.submitted = true;
-            if (form.fname.$valid) {
-                Auth.changeFirstName($scope.user.first_name)
-                    .then(() => {
-                        $scope.currentProgress = updateProgressBar();
-                    })
-                    .catch(err => {
-                        err = err.data;
-                        errors = {};
-
-                        // Update validity of form fields that match the mongoose errors
-                        angular.forEach(err.errors, (error, field) => {
-                            form[field].$setValidity('mongoose', false);
-                            errors[field] = error.message;
-                        });
-                    });
-            } else {
+            $rootScope.$on('user:changed', function()  {
                 $scope.currentProgress = updateProgressBar();
-            }
-        };
+            });
 
-        $scope.changeLastName = function(form) {
-            $scope.submitted = true;
-            if (form.lname.$valid) {
-                Auth.changeLastName($scope.user.last_name)
-                    .then(() => {
-                        $scope.currentProgress = updateProgressBar();
-                    })
-                    .catch(err => {
-                        err = err.data;
-                        errors = {};
-
-                        // Update validity of form fields that match the mongoose errors
-                        angular.forEach(err.errors, (error, field) => {
-                            form[field].$setValidity('mongoose', false);
-                            errors[field] = error.message;
-                        });
-                    });
-            } else {
-                $scope.currentProgress = updateProgressBar();
-            }
-        };
-
-        $scope.$on('user:changed', function()  {
-            $scope.currentProgress = updateProgressBar();
-        });
+            function updateProgressBar() {
 
 
-        function updateProgressBar() {
-            var progress = 25;
-            var currentUser = Auth.getCurrentUser();
-            if (currentUser.first_name || currentUser.last_name) {
-                progress = 35;
-            }
-            if (currentUser.first_name && currentUser.last_name) {
-                progress = 45;
-            }
-            if (currentUser.first_name && currentUser.last_name && currentUser.verified) {
-                progress = 55;
-            }
-            if (currentUser.verifiedPhone == false) {
-                progress = 75;
-            }
-            if (currentUser.verifiedPhone == true) {
-                progress = 90;
-            }
-            if (currentUser.weekdays && currentUser.weekdays.length > 0) {
-                progress = 100;
-                if (!currentUser.registrationCompleted) {
-                    $timeout(function() {
-                        Auth.setRegistrationCompleted(true);
-                    }, 1000);
+                /*
+                var progress = 25;
+                var currentUser = Auth.getCurrentUser();
+                if (currentUser.first_name || currentUser.last_name) {
+                    progress = 35;
                 }
-            }
-            return progress;
-        };
+                if (currentUser.first_name && currentUser.last_name) {
+                    progress = 45;
+                }
+                if (currentUser.first_name && currentUser.last_name && currentUser.verified) {
+                    progress = 55;
+                }
+                if (currentUser.verifiedPhone == false) {
+                    progress = 75;
+                }
+                if (currentUser.verifiedPhone == true) {
+                    progress = 90;
+                }
+                if (currentUser.weekdays && currentUser.weekdays.length > 0) {
+                    progress = 100;
+                    if (!currentUser.registrationCompleted) {
+                        $timeout(function() {
+                            Auth.setRegistrationCompleted(true);
+                        }, 1000);
+                    }
+                }
+                return progress;
+                
+            };
+
+*/
+
     });
