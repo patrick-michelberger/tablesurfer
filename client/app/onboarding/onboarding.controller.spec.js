efe2e8a07f9b5a87ab30f9dc1aea7368e8f25479
patrick_michelberger <pmichelberger@gmx.de>
2016-03-26 16:56:35 +0100

    unused specs removed

---
+++ client/app/onboarding/onboarding.controller.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Component: OnboardingComponent', function () {
+
+  // load the controller's module
+  beforeEach(module('tablesurferApp'));
+
+  var OnboardingComponent, scope;
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});
