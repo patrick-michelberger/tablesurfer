22d7a0d0f1c2d7c3901ad279846cd996465e8ba1
patrick_michelberger <pmichelberger@gmx.de>
2016-03-26 17:02:05 +0100

    changes

---
+++ client/components/tsName/tsName.directive.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module('tablesurferApp')
+    .directive('tsName', function(Auth, $rootScope, $timeout) {
+        return {
+            templateUrl: 'components/tsName/tsName.html',
+            restrict: 'EA',
+            link: function(scope, element, attrs) {
+                var currentUser = Auth.getCurrentUser();
+                var errors = {};
+
+                scope.user = Auth.getCurrentUser();
+                scope.getCurrentUser = Auth.getCurrentUser;
+
+                scope.changeFirstName = function(form) {
+                    if (form.fname.$valid) {
+                        Auth.changeFirstName(scope.user.first_name)
+                            .then(() => {
+                                $rootScope.$emit('user:changed');
+                            })
+                            .catch(err => {
+                                err = err.data;
+                                errors = {};
+
+                                // Update validity of form fields that match the mongoose errors
+                                angular.forEach(err.errors, (error, field) => {
+                                    form[field].$setValidity('mongoose', false);
+                                    errors[field] = error.message;
+                                });
+                            });
+                    } else {
+                        $rootScope.$emit('user:changed');
+                    }
+                };
+
+                scope.changeLastName = function(form) {
+                    if (form.lname.$valid) {
+                        Auth.changeLastName(scope.user.last_name)
+                            .then(() => {
+                                $rootScope.$emit('user:changed');
+                            })
+                            .catch(err => {
+                                err = err.data;
+                                errors = {};
+
+                                // Update validity of form fields that match the mongoose errors
+                                angular.forEach(err.errors, (error, field) => {
+                                    form[field].$setValidity('mongoose', false);
+                                    errors[field] = error.message;
+                                });
+                            });
+                    } else {
+                        $rootScope.$emit('user:changed');
+                    }
+                };
+            }
+        };
+    });
