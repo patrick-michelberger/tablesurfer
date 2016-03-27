fb456f6a1e75864b30a3e81bd8fb58cb40157bd7
patrick_michelberger <pmichelberger@gmx.de>
2016-03-27 01:19:39 +0100

    how it works added

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('tablesurferApp')
-    .controller('MainCtrl', function($scope, Auth) {
+    .controller('MainCtrl', function($rootScope, $scope, Auth) {
         $scope.isLoggedIn = Auth.isLoggedIn;
         $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.innerHeight = window.innerHeight - 50;
+
+        $scope.howToModal = false;
+
+        $scope.openHowToModal = function() {
+            $rootScope.Ui.turnOn('howToModal');
+
+        };
     });
