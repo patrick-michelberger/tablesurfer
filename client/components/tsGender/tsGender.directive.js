d4ce9429a36b8e16534a0d52808295d50f9ae11c
patrick_michelberger <pmichelberger@gmx.de>
2016-03-29 04:05:49 +0200

    gender added

---
+++ client/components/tsGender/tsGender.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('tablesurferApp')
+  .directive('tsGender', function () {
+    return {
+      templateUrl: 'components/tsGender/tsGender.html',
+      restrict: 'EA',
+      link: function (scope, element, attrs) {
+    
+      }
+    };
+  });
