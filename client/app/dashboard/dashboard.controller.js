2abc0e30bc3c1612f8bb21c8b484ad725627a7b0
patrick_michelberger <pmichelberger@gmx.de>
2016-03-29 02:52:27 +0200

    layout changes

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -3,12 +3,11 @@
 angular.module('tablesurferApp')
     .controller('DashboardCtrl', function($rootScope, $scope, $state, Auth) {
         // Check if user is completely registered 
-        /*Auth.getCurrentUser(function(currentUser) {
+        Auth.getCurrentUser(function(currentUser) {
             if (!Auth.getCurrentUser().registrationCompleted) {
                 $state.go('onboarding');
             }
         });
-        */
 
         $scope.getCurrentUser = Auth.getCurrentUser;
 
